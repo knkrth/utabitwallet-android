7ca168f42475f093f9f08826663021a8619c79be
karthik <kartx.rockeveryone@gmail.com>
2017-10-18 16:40:20 +0000

    Update BRAddress.h

--- app/src/main/jni/breadwallet-core/BRAddress.h
+++ app/src/main/jni/breadwallet-core/BRAddress.h
@@ -39,8 +39,8 @@ extern "C" {
 #endif
 
 // bitcoin address prefixes
-#define BITCOIN_PUBKEY_ADDRESS      0
-#define BITCOIN_SCRIPT_ADDRESS      5
+#define BITCOIN_PUBKEY_ADDRESS      111
+#define BITCOIN_SCRIPT_ADDRESS      196
 #define BITCOIN_PUBKEY_ADDRESS_TEST 111
 #define BITCOIN_SCRIPT_ADDRESS_TEST 196
 
